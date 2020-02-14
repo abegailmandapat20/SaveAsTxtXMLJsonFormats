80ecb5b3e2b28fdfa8bb8d0e88dae51ecec60991
abegailmandapat20 <58084229+abegailmandapat20@users.noreply.github.com>
2020-02-15 02:33:08 +0800

    Add files via upload

---
+++ SaveAsTxtXMLJson/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtBtn = New System.Windows.Forms.Button()
+        Me.XMLBtn = New System.Windows.Forms.Button()
+        Me.JsonBtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(93, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(91, 208)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 36)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Age"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(92, 313)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 36)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Address"
+        '
+        'TxtBtn
+        '
+        Me.TxtBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtBtn.Location = New System.Drawing.Point(119, 401)
+        Me.TxtBtn.Name = "TxtBtn"
+        Me.TxtBtn.Size = New System.Drawing.Size(81, 62)
+        Me.TxtBtn.TabIndex = 1
+        Me.TxtBtn.Text = "Txt"
+        Me.TxtBtn.UseVisualStyleBackColor = True
+        '
+        'XMLBtn
+        '
+        Me.XMLBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.XMLBtn.Location = New System.Drawing.Point(250, 401)
+        Me.XMLBtn.Name = "XMLBtn"
+        Me.XMLBtn.Size = New System.Drawing.Size(81, 62)
+        Me.XMLBtn.TabIndex = 1
+        Me.XMLBtn.Text = "XML"
+        Me.XMLBtn.UseVisualStyleBackColor = True
+        '
+        'JsonBtn
+        '
+        Me.JsonBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.JsonBtn.Location = New System.Drawing.Point(374, 401)
+        Me.JsonBtn.Name = "JsonBtn"
+        Me.JsonBtn.Size = New System.Drawing.Size(81, 62)
+        Me.JsonBtn.TabIndex = 1
+        Me.JsonBtn.Text = "Json"
+        Me.JsonBtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(237, 89)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(258, 57)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(237, 197)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(258, 57)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(237, 302)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(258, 57)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(19, 489)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(166, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Mandapat, Abegail A."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(19, 509)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(93, 20)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "BSCpE 5-3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 553)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.JsonBtn)
+        Me.Controls.Add(Me.XMLBtn)
+        Me.Controls.Add(Me.TxtBtn)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(600, 600)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(600, 600)
+        Me.Name = "Form1"
+        Me.Text = "SaveAsTxtXMLJson"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtBtn As Button
+    Friend WithEvents XMLBtn As Button
+    Friend WithEvents JsonBtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ SaveAsTxtXMLJson/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, TextBox3.TextChanged, TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TxtBtn_Click(sender As Object, e As EventArgs) Handles TxtBtn.Click
+        My.Computer.FileSystem.WriteAllText("C:\Sample\Textfile1.txt", "" & "Name: " + TextBox1.Text + Environment.NewLine & "Age: " + TextBox2.Text + Environment.NewLine & "Address: " + TextBox3.Text + Environment.NewLine, True)
+
+    End Sub
+
+    Private Sub XMLBtn_Click(sender As Object, e As EventArgs) Handles XMLBtn.Click
+        My.Computer.FileSystem.WriteAllText("C:\Sample\Textfile1.xml", "" & "Name: " + TextBox1.Text + Environment.NewLine & "Age: " + TextBox2.Text + Environment.NewLine & "Address: " + TextBox3.Text, True)
+    End Sub
+
+    Private Sub JsonBtn_Click(sender As Object, e As EventArgs) Handles JsonBtn.Click
+        My.Computer.FileSystem.WriteAllText("C:\Sample\Textfile1.json", "" & "Name: " + TextBox1.Text + Environment.NewLine & "Age: " + TextBox2.Text + Environment.NewLine & "Address: " + TextBox3.Text, True)
+    End Sub
+End Class
